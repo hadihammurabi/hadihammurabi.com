e88b4eb878e7e27abab7da5658a80094efebfba8
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-05-22 13:32:39 +0700

    ready to play

--- hadihammurabicom/settings/base.py
+++ hadihammurabicom/settings/base.py
@@ -91,11 +91,12 @@ WSGI_APPLICATION = 'hadihammurabicom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hadihammurabicom',
+        'USER': 'hammurabi',
+        'PASSWORD': 'hammurabi',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -158,6 +159,3 @@ MEDIA_URL = '/media/'
 
 WAGTAIL_SITE_NAME = "hadihammurabi"
 
-# Base URL to use when referring to full URLs within the Wagtail admin backend -
-# e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://example.com'
